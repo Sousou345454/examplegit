23374ba451cc9c56c3273d8cd0df8a53276a6175
Mehdi <faridsaloudo@gmail.com>
2022-02-05 00:01:57 +0100

    added fetch logic

--- main2.js
+++ main2.js
@@ -1,1 +1,10 @@
 const main2 = "hello" 
+
+async function someotherfunction() {
+	let resp = await fetch()
+	//TODO get response data and store it in the db.
+}
+
+const main2 = async () => {
+	await someotherfunction()
+}
